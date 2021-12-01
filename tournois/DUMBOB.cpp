c66aa1d51c8858c95b64798150fb66e1eb58c94f
FreeZixDG <94748010+FreeZixDG@users.noreply.github.com>
2021-12-01 02:39:12 +0100

    good ia

--- tournois/DUMBOB.cpp
+++ tournois/DUMBOB.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 // actual best: 27408 pts
+// actual best: 35892 pts
+// actual best: 36912 pts
 
 vector<int> extract_numbers(string s)
 {
@@ -175,29 +177,27 @@ char convertDirection(int v)
  * @param first_direction int, the first direction
  * @return the end score
 */
-int play_game(Plateau plateau, int first_direction)
+int play_game(Plateau plateau, int first_direction, int moves_ahead)
 {
 
     int direction = first_direction;
-    int moves = 20;
     do
     {
         plateau = deplacement(plateau, direction);
         direction = choose_random_move();
-        moves--;
-    } while (moves != 0);
+        moves_ahead--;
+    } while (moves_ahead != 0);
 
     return plateau.score;
 }
 
 
-char strat_brute(Plateau plateau)
+char strat_brute(Plateau plateau, int number_of_games, int moves_ahead)
 {
     int moyenne_G = 0;
     int moyenne_D = 0;
     int moyenne_H = 0;
     int moyenne_B = 0;
-    int games = 1000;
 
     /*test if the position is valid*/
     if (deplacement(plateau, GAUCHE).grille == plateau.grille) moyenne_G = -1;
@@ -205,12 +205,12 @@ char strat_brute(Plateau plateau)
     if (deplacement(plateau, HAUT).grille == plateau.grille) moyenne_H = -1;
     if (deplacement(plateau, BAS).grille == plateau.grille) moyenne_B = -1;
 
-    for (int k = 0; k < games / 4; k++)
+    for (int k = 0; k < number_of_games / 4; k++)
     {
-        if (moyenne_G != -1) moyenne_G += play_game(plateau, GAUCHE);
-        if (moyenne_D != -1) moyenne_D += play_game(plateau, DROITE);
-        if (moyenne_H != -1) moyenne_H += play_game(plateau, HAUT);
-        if (moyenne_B != -1) moyenne_B += play_game(plateau, BAS);
+        if (moyenne_G != -1) moyenne_G += play_game(plateau, GAUCHE, moves_ahead);
+        if (moyenne_D != -1) moyenne_D += play_game(plateau, DROITE, moves_ahead);
+        if (moyenne_H != -1) moyenne_H += play_game(plateau, HAUT, moves_ahead);
+        if (moyenne_B != -1) moyenne_B += play_game(plateau, BAS, moves_ahead);
     } 
 
     int max_score = max(max(moyenne_G, moyenne_D), max(moyenne_H, moyenne_B));
@@ -227,7 +227,7 @@ char strat_brute(Plateau plateau)
 int main()
 {
     srand(time(NULL));
-    int actual_try = 797;
+    int actual_try = 0;
     char prev_move = '\0';
     char rep;
     int tries;
@@ -237,11 +237,12 @@ int main()
 
     while (1)
     {
+        
         do
         {
             printf("\rWaiting for game update n°%d", actual_try);
             fflush(stdout);
-            this_thread::sleep_for(chrono::milliseconds(100));
+            this_thread::sleep_for(chrono::milliseconds(2));
             plateau = readInfo("configuration.txt", tries, score);
         }
         while(tries != actual_try);
@@ -255,7 +256,12 @@ int main()
         if (tries == actual_try)
         {
             printf("\n");
-            rep = strat_brute(plateau);
+            if (plateau.score < 10000) rep = strat_brute(plateau, 50, 10);
+            else if (plateau.score < 15000) rep = strat_brute(plateau, 100, 10);
+            else if (plateau.score < 25000) rep = strat_brute(plateau, 500, 20);
+            else if (plateau.score < 30000) rep = strat_brute(plateau, 1000, 30);
+            else rep = strat_brute(plateau, 2000, 50);
+            
             writeMove("mouvements.txt", "BOB", tries, rep);
             printf("updated!\n");
             printf("Wrote movement %c\n", rep);
@@ -264,6 +270,7 @@ int main()
 
 
         printf("try: %d\nscore: %d\n", tries, score);
+        system("clear");
         printf("%s", dessine(plateau).c_str());
     }
 
